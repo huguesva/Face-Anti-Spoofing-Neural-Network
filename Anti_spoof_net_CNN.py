024b8c0246fadd91560907b84b153771bfc30117
hva <huguesva@orange.fr>
2020-02-22 13:35:25 +0100

    add block class

--- Anti_spoof_net_CNN.py
+++ Anti_spoof_net_CNN.py
@@ -3,26 +3,15 @@ import torch.nn as nn
 import numpy as np
 import torchvision
 
-class Anti_spoof_net_CNN(nn.Module):
-  
-    def __init__ (self):
-        super(Anti_spoof_net_CNN,self).__init__()
-            
-        self.resize_32 = nn.Upsample(size=32, mode='nearest')
-        self.resize_64 = nn.Upsample(size=64, mode='nearest')
+class block(nn.Module):
 
-        self.cnn0=nn.Conv2d(in_channels=3,out_channels=64, kernel_size=3,stride=1,padding=1)
-        nn.init.xavier_normal(self.cnn0.weight) 
-        self.bn0=nn.BatchNorm2d(64)
-        self.non_linearity0=nn.CELU(alpha=1.0, inplace=False)
-        
-        self.cnn01=nn.Conv2d(in_channels=64,out_channels=128, kernel_size=3,stride=1,padding=1)
-        nn.init.xavier_normal(self.cnn01.weight) 
-        self.bn01=nn.BatchNorm2d(128)
-        self.non_linearity01=nn.CELU(alpha=1.0, inplace=False)
+    def __init__(self,begin):
+        super(block,self).__init__()
 
-        #Block:
-        self.cnn1=nn.Conv2d(in_channels=128,out_channels=128, kernel_size=3,stride=1,padding=1)
+        if begin==True:
+            self.cnn1=nn.Conv2d(in_channels=64,out_channels=128, kernel_size=3,stride=1,padding=1)
+        else:
+            self.cnn1=nn.Conv2d(in_channels=128,out_channels=128, kernel_size=3,stride=1,padding=1)
         nn.init.xavier_normal(self.cnn1.weight) 
         self.bn1=nn.BatchNorm2d(128)
         self.non_linearity1=nn.CELU(alpha=1.0, inplace=False)
@@ -38,6 +27,37 @@ class Anti_spoof_net_CNN(nn.Module):
         self.non_linearity3=nn.CELU(alpha=1.0, inplace=False)
         
         self.pool=nn.MaxPool2d(kernel_size=2)
+
+    def forward(self,x,begin):
+        
+        x=self.cnn1(x)
+        x=self.bn1(x)
+        x=self.non_linearity1(x)
+        x=self.cnn2(x)
+        x=self.bn2(x)
+        x=self.non_linearity2(x)
+        x=self.cnn3(x)
+        x=self.bn3(x)
+        x=self.non_linearity3(x)
+        x=self.pool(x)
+    
+
+class Anti_spoof_net_CNN(nn.Module):
+  
+    def __init__ (self):
+        super(Anti_spoof_net_CNN,self).__init__()
+            
+        self.resize_32 = nn.Upsample(size=32, mode='nearest')
+        self.resize_64 = nn.Upsample(size=64, mode='nearest')
+
+        self.cnn0=nn.Conv2d(in_channels=3,out_channels=64, kernel_size=3,stride=1,padding=1)
+        nn.init.xavier_normal(self.cnn0.weight) 
+        self.bn0=nn.BatchNorm2d(64)
+        self.non_linearity0=nn.CELU(alpha=1.0, inplace=False)
+
+        self.block1=block(True)
+        self.block2=block(False)
+        self.block3=block(False)
         
         #Feature map:
         self.cnn4=nn.Conv2d(in_channels=384,out_channels=128, kernel_size=3,stride=1,padding=1)
@@ -48,8 +68,9 @@ class Anti_spoof_net_CNN(nn.Module):
         self.cnn7=nn.Conv2d(in_channels=384,out_channels=128, kernel_size=3,stride=1,padding=1)
         self.cnn8=nn.Conv2d(in_channels=128,out_channels=64, kernel_size=3,stride=1,padding=1)
         self.cnn9=nn.Conv2d(in_channels=64,out_channels=1, kernel_size=3,stride=1,padding=1)
-        
-        
+
+
+
     def forward(self,x):
         
         x=self.cnn0(x)
@@ -57,45 +78,15 @@ class Anti_spoof_net_CNN(nn.Module):
         x=self.non_linearity0(x)
         
         #Block1
-        x=self.cnn01(x)
-        x=self.bn01(x)
-        x=self.non_linearity01(x)
-        x=self.cnn2(x)
-        x=self.bn2(x)
-        x=self.non_linearity2(x)
-        x=self.cnn3(x)
-        x=self.bn3(x)
-        x=self.non_linearity3(x)
-        x=self.pool(x)
-        
+        x=self.block1(x)
         X1=self.resize_64(x)
         
         #Block2
-        x=self.cnn1(x)
-        x=self.bn1(x)
-        x=self.non_linearity1(x)
-        x=self.cnn2(x)
-        x=self.bn2(x)
-        x=self.non_linearity2(x)
-        x=self.cnn3(x)
-        x=self.bn3(x)
-        x=self.non_linearity3(x)
-        x=self.pool(x)
-        
+        x=self.block2(x)
         X2=x
         
         #Block3:
-        x=self.cnn1(x)
-        x=self.bn1(x)
-        x=self.non_linearity1(x)
-        x=self.cnn2(x)
-        x=self.bn2(x)
-        x=self.non_linearity2(x)
-        x=self.cnn3(x)
-        x=self.bn3(x)
-        x=self.non_linearity3(x)
-        x=self.pool(x)
-        
+        x=self.block3(x)
         X3=self.resize_64(x)
         
         X=torch.cat((X1,X2,X3),1)
